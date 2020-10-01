284cd746ea0a0142e960f69ef87b68eb606e084e
michaelwooley <michael@guhdoh.com>
2020-10-01 17:02:11 -0400

    Install apps, add bulma and fontawesome

---
+++ src/fontawesome.ts
@@ -0,0 +1,13 @@
+// import the library
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+
+// import your icons
+import { faCode, faHighlighter } from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faCode,
+  faHighlighter
+  // more icons go here
+);
+
+dom.watch();
