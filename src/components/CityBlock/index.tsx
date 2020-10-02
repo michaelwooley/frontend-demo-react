4036d8902ccf10bc8177ee873ccbf88daf90b07a
michaelwooley <michael@guhdoh.com>
2020-10-02 16:16:49 -0400

    a11y-compatible tabs styling

--- src/components/CityBlock/index.tsx
+++ src/components/CityBlock/index.tsx
@@ -1,85 +1,123 @@
-import React from "react";
-import { APP_NAME } from "common/constants";
+import React, { useState } from "react";
+import { IStationName } from "common/data/stations";
 import "./index.scss";
+import { useIsTablet } from "common/hooks";
+
+const BodyTabs: React.FC<{
+  currentComponent: React.ReactNode;
+  chartComponent: React.ReactNode;
+}> = ({ currentComponent, chartComponent }) => {
+  type tabIds = "current" | "chart";
+  const [activeTab, setActiveTab] = useState<tabIds>("current");
+
+  const tabs: { id: tabIds; icon: string; label: string }[] = [
+    { id: "current", icon: "fas fa-thermometer", label: "Current" },
+    { id: "chart", icon: "fas fa-chart-line", label: "Charts" },
+  ];
+
+  return (
+    <>
+      <div className="tabs as-button is-centered is-small">
+        <ul>
+          {tabs.map((tab) => (
+            <li
+              key={tab.id}
+              className={activeTab === tab.id ? "is-active" : ""}
+            >
+              <button
+                className="button is-small"
+                onClick={() => setActiveTab(tab.id)}
+              >
+                <span className="icon is-small">
+                  <i className={tab.icon} aria-hidden="true"></i>
+                </span>
+                <span>{tab.label}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        {activeTab === "current" && currentComponent}
+        {activeTab === "chart" && chartComponent}
+      </div>
+    </>
+  );
+};
 
 export interface CityBlockProps {
   /**
-   * If true, menu will be open in case of mobile
+   * Station data
    */
-  isActive: boolean;
+  station: IStationName;
 
   /**
-   * Toggles visibility of mobile menu
+   * If true, the full panel is displayed
    */
-  onToggleActive: () => void;
+  isOpen: boolean;
 
   /**
-   * Toggles city selector modal
+   * Called to toggle panel open or close.
    */
-  onToggleCities: () => void;
+  onToggleOpen: (station: IStationName) => void;
+
+  /**
+   * Displays dropdown menu on right of top bar.
+   */
+  dropdownComponent?: React.ReactNode;
 }
 
 /**
  * Primary UI component for user interaction
  */
 export const CityBlock: React.FC<CityBlockProps> = ({
-  isActive = true,
-  onToggleCities,
-  onToggleActive,
+  station,
+  isOpen = false,
+  onToggleOpen,
+  dropdownComponent,
+  ...props
 }) => {
+  const isTablet = useIsTablet();
+
   return (
-    <nav
-      id="app-navbar"
-      className="navbar"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="navbar-brand">
-        <div className="navbar-item is-selectable">
-          <span className="icon has-text-primary">
-            <i className="fa fa-cloud"></i>
-          </span>
-          <span className="ml-2 has-text-weight-bold">{APP_NAME}</span>
-        </div>
-        <div
-          role="button"
-          className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-          onClick={onToggleActive}
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </div>
-      </div>
-
-      <div
-        id="navbarBasicExample"
-        className={`navbar-menu ${isActive ? "is-active" : ""}`}
-      >
-        {/* 
-        <div className="navbar-start">
-          <a className="navbar-item">About</a> 
-        </div>*/}
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <button
-                className="button is-link is-outlined is-fullwidth"
-                onClick={onToggleCities}
-              >
-                <span className="icon">
-                  <i className="fa fa-city"></i>
-                </span>
-                <span className="has-text-weight-semibold">Cities</span>
-              </button>
-            </div>
+    <div className="city-block mb-2" {...props}>
+      <nav className="level mb-2">
+        <div className="level-left">
+          <div className="level-item">
+            <button
+              className="button is-medium is-white"
+              onClick={() => onToggleOpen(station)}
+            >
+              <span className="icon">
+                <i
+                  className={`fas ${
+                    isOpen ? "fa-chevron-up" : "fa-chevron-down"
+                  }`}
+                ></i>
+              </span>
+              <span className="has-text-weight-semibold">{station.city}</span>
+            </button>
           </div>
         </div>
-      </div>
-    </nav>
+
+        <div className="level-right">
+          {dropdownComponent && (
+            <div className="level-item">{dropdownComponent}</div>
+          )}
+        </div>
+      </nav>
+      {isOpen &&
+        (isTablet ? (
+          <div className="columns">
+            <div className="column is-narrow">left</div>
+            <div className="column">right</div>
+          </div>
+        ) : (
+          <BodyTabs
+            currentComponent={<div>current</div>}
+            chartComponent={<div>chart</div>}
+          ></BodyTabs>
+        ))}
+    </div>
   );
 };
