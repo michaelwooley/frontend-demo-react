1c0a4ae33f1f17957adb1d172f23ca6d7f976a13
michaelwooley <michael@guhdoh.com>
2020-10-02 14:23:26 -0400

    New icons + styling for bulma

--- src/common/fontawesome.ts
+++ src/common/fontawesome.ts
@@ -2,9 +2,14 @@
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 
 // import your icons
-import { faCloud, faCity } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCloud,
+  faCity,
+  faChevronDown,
+  faChevronUp,
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faCloud, faCity);
+library.add(faCloud, faCity, faChevronDown, faChevronUp);
 
 // Replaces <i /> elements with svg.
 dom.watch();
