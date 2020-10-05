3dd3d29b77a5f15cbe7cf21e6be0ed7e186eaf27
michaelwooley <michael@guhdoh.com>
2020-10-05 18:07:31 -0400

    Add helper functions from useArrayReducer

--- src/hooks/useStations.ts
+++ src/hooks/useStations.ts
@@ -5,13 +5,20 @@ import useArrayReducer from "./useArrayReducer";
 const KEY = LOCAL_STORAGE_KEYS.STATIONS;
 
 export function useStations() {
-  const { state: stations, onAdd, onRemove, onMove } = useArrayReducer<
-    IStationName
-  >(JSON.parse(localStorage.getItem(KEY) || JSON.stringify([])));
+  const {
+    state: stations,
+    onAdd,
+    onRemove,
+    onMove,
+    onMoveDown,
+    onMoveUp,
+  } = useArrayReducer<IStationName>(
+    JSON.parse(localStorage.getItem(KEY) || JSON.stringify([]))
+  );
 
   useEffect(() => {
     localStorage.setItem(KEY, JSON.stringify(stations));
   }, [stations]);
 
-  return { stations, onAdd, onMove, onRemove };
+  return { stations, onAdd, onMove, onRemove, onMoveDown, onMoveUp };
 }
