59689ed647dc9787c3ddec164b7e4fd96e5f58f8
michaelwooley <michaelwooley@users.noreply.github.com>
2020-10-19 13:02:33 +0000

    deploy: 47ecd96c28b4390c627c485b165491352525b4fa

---
+++ precache-manifest.fb92e8b49b0c63303651003835732e96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d9d4e1efc8f4d1bb7039672d0b4932",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c15d77d7fa0ef669a37f",
+    "url": "/static/css/main.bad6192a.chunk.css"
+  },
+  {
+    "revision": "2ccda520429357c4541b",
+    "url": "/static/js/2.0f89060f.chunk.js"
+  },
+  {
+    "revision": "bc66ae6bda8b4cde76fa3534f76de364",
+    "url": "/static/js/2.0f89060f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c15d77d7fa0ef669a37f",
+    "url": "/static/js/main.f572ec7d.chunk.js"
+  },
+  {
+    "revision": "3d6c861e45b57a16db93",
+    "url": "/static/js/runtime-main.97f2c405.js"
+  }
+]);
